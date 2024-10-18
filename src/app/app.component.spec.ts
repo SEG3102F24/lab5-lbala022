0b146af17ad5f990e987c94a20cfbfc12ec7eecc
Luca Balan <113710003+lbala022@users.noreply.github.com>
2024-10-18 10:53:11 +0300

    branch fix

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,16 +13,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'employee-app'`, () => {
+  it(`should have as title 'employeeApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('employee-app');
+    expect(app.title).toEqual('employeeApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('employee-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('employeeApp app is running!');
   });
 });
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,16 +2,7 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import {environment} from "../environments/environment.development";
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    //provideFirebaseApp(() => initializeApp({"projectId":"my-employee-app-cf155","appId":"1:336430001389:web:38c42a636586b33db770e4","storageBucket":"my-employee-app-cf155.appspot.com","apiKey":"AIzaSyADyE-qI1VpOxtUR2JU1F_J6KHNX9zR7Us","authDomain":"my-employee-app-cf155.firebaseapp.com","messagingSenderId":"336430001389","measurementId":"G-29GW16YC65"})),
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideFirestore(() => getFirestore())
-  ]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
 };
